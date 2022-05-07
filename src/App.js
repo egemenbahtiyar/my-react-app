783e0215a82c8c3c15f106edd3f2e2c678e84ffc
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-05-07 16:25:31 +0300

    props was used.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+    const dataList = [
+        {
+            id: 1,
+            title: "Toilet paper",
+            amount: 2,
+            price: "$99",
+        },
+        {
+            id: 2,
+            title: "Book",
+            amount: 2,
+            price: "$20",
+        },
+    ];
     return (
         <div>
-            Lets get Started !<ExpenseItem></ExpenseItem>
+            <ExpenseItem
+                title={dataList[0].title}
+                amount={dataList[0].amount}
+                price={dataList[0].price}
+            ></ExpenseItem>
+            <ExpenseItem
+                title={dataList[1].title}
+                amount={dataList[1].amount}
+                price={dataList[1].price}
+            ></ExpenseItem>
         </div>
     );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,15 @@
 import "./ExpenseItem.css";
-function ExpenseItem() {
-    let expenseDate = new Date(2022, 5, 7);
-    let expenseDescription = "Car Insurance";
-    let expensePrice = "198.67";
+function ExpenseItem(props) {
+    // let expenseDate = new Date(2022, 5, 7);
+    // let expenseDescription = "Car Insurance";
+    // let expensePrice = "198.67";
     return (
         <div className="expense-item">
             <div>
-                <div>{expenseDate.toISOString()}</div>
+                <div>{props.title}</div>
                 <div className="expense-item__description">
-                    <h2>{expenseDescription}</h2>
-                    <div className="expense-item__price">${expensePrice}</div>
+                    <h2>{props.amount}</h2>
+                    <div className="expense-item__price">{props.price}</div>
                 </div>
             </div>
         </div>
