fda3e23e709cb023fa3b211bbe639ec447267e53
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-05-14 16:03:00 +0300

    titlechange method was added.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,15 @@
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+    const TitleChangeHandler = (event) => {
+        console.log(event.target.value);
+    };
     return (
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" />
+                    <input type="text" onChange={TitleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
